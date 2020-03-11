c1e541df93b894611f7177f3c3e2ad5e8d7f2994
Vu Pham <vuph79@gmail.com>
2020-03-11 11:59:51 +0700

    Fix url of redis connection to match Heroku Redis

--- src/utils/redis.js
+++ src/utils/redis.js
@@ -1,7 +1,6 @@
 import redis from "redis";
-import { promisify } from "util";
 
-const client = redis.createClient({ port: 6379 });
+const client = redis.createClient(process.env.REDIS_URL);
 
 client.on("error", function(error) {
   console.error(error);
