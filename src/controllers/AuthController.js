f29130e2a40ca151eb602cee9dbf1b8ede798558
Vu Pham <vuph79@gmail.com>
2020-03-19 16:27:38 +0700

    Update: Change redis into module function

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import { client as redis } from "../utils/redis_";
+import { client as redis, checkTokenExist } from "../utils/redis_";
 
 export async function authAdmin(req, res, next) {
   try {
@@ -8,12 +8,13 @@ export async function authAdmin(req, res, next) {
       process.env.secret
     );
     if (admin) {
-      redis.get(req.signedCookies["adminToken"], (err, reply) => {
-        if (reply) {
-          req.admin = admin;
-          next();
-        } else return res.status(401).send("Not Authenticated!");
-      });
+      const exist = await checkTokenExist(req.signedCookies["adminToken"]);
+      if (exist) {
+        req.admin = admin;
+        next();
+      } else {
+        return res.status(401).send("Not Authenticated!");
+      }
     } else {
       return res.status(401).send("Not Authenticated!");
     }
@@ -26,12 +27,13 @@ export async function authUser(req, res, next) {
   try {
     const user = jwt.verify(req.signedCookies["userToken"], process.env.secret);
     if (user) {
-      redis.get(req.signedCookies["userToken"], (err, reply) => {
-        if (reply) {
-          req.user = user;
-          next();
-        } else return res.status(401).send("Not Authenticated!");
-      });
+      const exist = await checkTokenExist(req.signedCookies["userToken"]);
+      if (exist) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(401).send("Not Authenticated!");
+      }
     } else {
       return res.status(401).send("Not Authenticated!");
     }
@@ -42,20 +44,11 @@ export async function authUser(req, res, next) {
 
 export async function authAll(req, res, next) {
   try {
-    let auth = false;
-    redis.get(req.signedCookies["adminToken"], (err, reply) => {
-      if (reply) {
-        auth = true;
-      }
-      redis.get(req.signedCookies["userToken"], (err, reply) => {
-        if (reply) {
-          auth = true;
-        }
-        if (auth) {
-          next();
-        } else return res.status(401).send("Not Authenticated!");
-      });
-    });
+    const existUser = await checkTokenExist(req.signedCookies["userToken"]);
+    const existAdmin = await checkTokenExist(req.signedCookies["adminToken"]);
+    if (existAdmin || existUser) {
+      next();
+    } else return res.status(401).send("Not Authenticated!");
   } catch (error) {
     return res.status(401).send("Not Authenticated!");
   }
